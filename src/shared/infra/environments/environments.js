ab3f0e877d7ca40e22c9eefd7ef1387995522a9a
Gabriel Toledo <gabrielreb7@gmail.com>
2025-03-10 16:35:21 -0300

    ajuste

--- src/shared/infra/environments/environments.js
+++ src/shared/infra/environments/environments.js
@@ -34,8 +34,8 @@ export const initializeDatabase = async () => {
 
 
 const options = {
-    key: fs.readFileSync('/etc/letsencrypt/live/api.gestao.datatechsistemas.com.br/privkey.pem'),  // Chave privada do Certbot
-    cert: fs.readFileSync('/etc/letsencrypt/live/api.gestao.datatechsistemas.com.br/fullchain.pem'), // Certificado do Certbot
+    //key: fs.readFileSync('/etc/letsencrypt/live/api.gestao.datatechsistemas.com.br/privkey.pem'),  // Chave privada do Certbot
+    //cert: fs.readFileSync('/etc/letsencrypt/live/api.gestao.datatechsistemas.com.br/fullchain.pem'), // Certificado do Certbot
 };
 
 // Função para iniciar o servidor

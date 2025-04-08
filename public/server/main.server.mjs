4007828c832a7b0964c8c57af6bd0f2b319e2772
Gabriel Toledo <gabrielreb7@gmail.com>
2025-04-08 17:59:45 -0300

    ajuste campo e pastas

--- public/server/main.server.mjs
+++ public/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-3LYVNPZZ.mjs";import"./chunk-REENG7HS.mjs";import"./chunk-I23EP5SD.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-J7ITRECF.mjs";import"./chunk-REENG7HS.mjs";import"./chunk-I23EP5SD.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};

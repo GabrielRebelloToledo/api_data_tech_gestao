3cea118b3dfad46ac19e56e5293383253aed81e6
Gabriel Toledo <gabrielreb7@gmail.com>
2025-05-26 11:21:03 -0300

    retirando os logs

--- public/server/main.server.mjs
+++ public/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-FFPO2ISG.mjs";import"./chunk-LKDKBLXM.mjs";import"./chunk-I23EP5SD.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-DVB42PEB.mjs";import"./chunk-LKDKBLXM.mjs";import"./chunk-I23EP5SD.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};

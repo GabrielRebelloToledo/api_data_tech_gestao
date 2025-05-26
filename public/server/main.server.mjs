9b8d8dbf4d60c81f4367a4c205961b90893f49ed
Gabriel Toledo <gabrielreb7@gmail.com>
2025-05-26 11:05:23 -0300

    logout

--- public/server/main.server.mjs
+++ public/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-RL2FFH7R.mjs";import"./chunk-LKDKBLXM.mjs";import"./chunk-I23EP5SD.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-FFPO2ISG.mjs";import"./chunk-LKDKBLXM.mjs";import"./chunk-I23EP5SD.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
